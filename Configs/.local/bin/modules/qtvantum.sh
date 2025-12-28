dc3d6f83dbbe9522161908c4be92ce595316d3fd
Ivy <121424993+IvyProtocol@users.noreply.github.com>
2025-12-28 15:45:21 +0600

    Update qtvantum.sh

--- Configs/.local/bin/modules/qtvantum.sh
+++ Configs/.local/bin/modules/qtvantum.sh
@@ -6,3 +6,14 @@ if [ ! -z ${dpid} ] ; then
     hyprctl dispatch closewindow pid:${dpid}
     hyprctl dispatch exec dolphin &
 fi
+
+if pgrep -f "polkit-gnome-authentication-agent-1" > /dev/null; then
+    # Kill existing agent properly
+    pkill -f "polkit-gnome-authentication-agent-1"
+fi
+    hyprctl dispatch exec "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1" >/dev/null 2>&1 &
+
+if pgrep -f "xdg-desktop-portal-gtk" > /dev/null; then
+    pkill -f "xdg-desktop-portal-gtk"
+fi
+    hyprctl dispatch exec "/usr/lib/xdg-desktop-portal-gtk" >/dev/null 2>&1 &
