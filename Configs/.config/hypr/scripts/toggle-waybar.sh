8064b4ebe0032ac1863f024483b0f2ab23cf94e2
Ivy <121424993+IvyProtocol@users.noreply.github.com>
2025-12-29 13:06:22 +0600

    Update toggle-waybar.sh

--- Configs/.config/hypr/scripts/toggle-waybar.sh
+++ Configs/.config/hypr/scripts/toggle-waybar.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 if pgrep -x "waybar" > /dev/null; then
-    pkill -SIGUSR2 waybar &>/dev/null
+    {
+        pkill -SIGUSR2 waybar &>/dev/null
+        swaync-client -rs
+    } >/dev/null 2>&1
 else
     {
         waybar
+        swaync-client -rs
     } >/dev/null 2>&1
     notify-send "Waybar launched"
 fi
